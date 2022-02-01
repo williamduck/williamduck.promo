ebd950424770c281d1ed73998f8a796a9294ed67
Haichen Zhao <haichen.zhao@shopee.com>
2022-02-01 20:13:28 +0800

    Insertion Sort

---
+++ sorts/InsertionSort.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding:UTF-8 -*-
+# @Time     : 1/2/22 7:57 PM
+# @Author   : solomon_mail@hotmail.com
+# @File     : InsertionSort.py
+
+
+class InsertionSortClass(object):
+    @staticmethod
+    def InsertionSort(input_list):
+        n = len(input_list)
+        for i in range(n):
+            tmp = input_list[i]
+            for j in range(i - 1, -2, -1):
+                if j != -1 and input_list[j] > tmp:
+                    input_list[j + 1] = input_list[j]
+                else:
+                    input_list[j + 1] = tmp
+                    break
+        return input_list
+
+
+if __name__ == "__main__":
+    sorted_list = InsertionSortClass.InsertionSort([6,1,8,2,0,33,1,5,1,1,2,4])
+    print(sorted_list)
